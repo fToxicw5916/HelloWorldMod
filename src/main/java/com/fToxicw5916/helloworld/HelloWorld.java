8b68006d4567a0e81b07fed351ae146c651cde46
fToxicw5916 <ftoxicw@163.com>
2022-05-04 12:25:38 +0800

    Added files

---
+++ src/main/java/com/fToxicw5916/helloworld/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.fToxicw5916.helloworld;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = HelloWorld.MODID, name = HelloWorld.NAME, version = HelloWorld.VERSION)
+public class HelloWorld
+{
+    public static final String MODID = "helloworld";
+    public static final String NAME = "Hello World";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // Hello world!
+        logger.info("Hello world!");
+    }
+}
